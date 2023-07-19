365979d8200b72edf3365fbc7eb2aadef7a92302
AAdler <amadler.dev@gmail.com>
2023-07-19 17:30:02 +0300

    add text by every submit

--- src/FormNew.js
+++ src/FormNew.js
@@ -3,31 +3,29 @@ import { useState } from "react";
 function FormNew() {
 
     const [input, setInput] = useState("");
-    // console.log('input: '+ input);
 
     const onSubmit = async (event) => {
         event.preventDefault();
-        // add here 
+
+        const value = document.querySelector('.value');
+        const element = <div>{input}</div>;
+        value.appendChild(document.createTextNode(input));
     };
 
     const onChange = (event) => {
-        console.log('input: '+ input);
-        console.log(event.target)
         const value = event.target.value;
         setInput(value);
     };
     
     return (
         <form onSubmit={onSubmit}>
-            TO DO LIST
+            <div className="">TO DO LIST</div>
             <label>
                 <div>Enter your task:</div>
                 <input type="text" onChange={onChange} value={input} />
             </label>
-            <div>
-                <button type="submit">Submit</button>
-                <div className="">{input}</div>
-            </div>
+            <button type="submit">Submit</button>
+            <div className="value"></div>
         </form>
     )
 }
