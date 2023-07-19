ed0720742620ff3b1392881c59f899d62a4f4ac0
AAdler <amadler.dev@gmail.com>
2023-07-19 23:18:07 +0300

    finish first logic

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# basic To Do List For Learning React
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import xIcon from './x-icon.svg';
 import './App.css';
 
 import { useState } from "react";
@@ -8,12 +9,29 @@ function App() {
     const [input, setInput] = useState("");
 
     const onSubmit = async (event) => {
+
+        // disable render when submit
         event.preventDefault();
 
+        // create task div
         const parent = document.querySelector('.value');
         const elem = document.createElement("div");
+
+        // add delete icon
+        const deleteIcon = document.createElement("img");
+        deleteIcon.setAttribute('src', xIcon)
+        deleteIcon.setAttribute('class', 'x-icon');
+
+        // handle remove task
+        deleteIcon.addEventListener(('click'), function() {
+            elem.remove();
+            deleteIcon.remove();
+        });
+
+        // append task
         elem.innerHTML = input + "\n"; // add delete icon
         parent.appendChild(elem);
+        parent.appendChild(deleteIcon);
     };
 
     const onChange = (event) => {
@@ -21,24 +39,22 @@ function App() {
         setInput(value);
     };
     
+    
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <form onSubmit={onSubmit}>
-            <div className="">TO DO LIST</div>
-            <label>
-                <div>Enter your task:</div>
-                <input type="text" onChange={onChange} value={input} />
-            </label>
-            <button type="submit">Submit</button>
-            <div className="value"></div>
-            {/* {elements.map((item) => <p>{item}</p>)} */}
-            
-        </form>
+            <img src={logo} className="App-logo" alt="logo" />
+            <form onSubmit={onSubmit}>
+                <div className="">TO DO LIST</div>
+                <label>
+                    <div>Enter your task:</div>
+                    <input type="text" onChange={onChange} value={input} />
+                </label>
+                <button type="submit">Submit</button>
+                <div className="value"></div>
+            </form>
         </header>
       </div>
-        
     )
 }
 
